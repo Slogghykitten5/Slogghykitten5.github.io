62b80c45ced8ead628ecb7785b32074977f3e0a8
Slogghykitten5 <71184690+Slogghykitten5@users.noreply.github.com>
2020-11-30 12:09:36 +0530

    Update main2.js

--- gallery-start/main2.js
+++ gallery-start/main2.js
@@ -103,9 +103,10 @@ but_left.onclick = function(){
     check()
 }
 
-    img_bg.onmouseover = function(){
+    img_bg.onmouseover = function(){/*
         but_left.style.zIndex = '1'
-        but_right.style.zIndex = '1'
+        but_right.style.zIndex = '1'*/
+        but_left.style.backgrounImage = 'linear-gradient(90deg, rgba(0,0,0,0) 0%, rgba(0,0,0,0.6) 100%),url("Icons/arrow-right-01-256.webp")center / 100px 120px no-repeat;'
     }
 
     img_bg.onmouseout = function(){
